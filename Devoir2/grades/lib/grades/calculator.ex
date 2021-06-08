c600bc80ac294bef6ae48657c42aaccf1bbe433e
Alex Degrace <adegr091@uottawa.ca>
2021-06-08 16:00:10 -0400

    Refactor for Q2.1

--- Devoir2/grades/lib/grades/calculator.ex
+++ Devoir2/grades/lib/grades/calculator.ex
@@ -1,37 +1,24 @@
 defmodule Grades.Calculator do
+  
+  def avg(marks) do
+    if Enum.count(marks) == 0 do
+        0
+    else
+      Enum.sum(marks) / Enum.count(marks)
+    end
+  end
+
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
-
-    mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+    mark = 0.2 * avg_homework + 0.3 * avg_labs + 0.2 * midterm + 0.3 * final
     round(mark * 100)
   end
 
   def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     avg_exams = (midterm + final) / 2
 
@@ -62,19 +49,8 @@ defmodule Grades.Calculator do
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     avg_exams = (midterm + final) / 2
 
