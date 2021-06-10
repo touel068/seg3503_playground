316927b1f5c3e527eea1037c23ae58eefe79d6a5
Thomas Ouellette <43257395+touel068@users.noreply.github.com>
2021-06-09 22:40:23 -0400

    Commit 2: success
    .
    +-- JUnit Jupiter [OK]
    | '-- TicTest [OK]
    |   '-- test1() [OK]
    '-- JUnit Vintage [OK]

--- lab04/tic_java/src/Tic.java
+++ lab04/tic_java/src/Tic.java
@@ -2,14 +2,15 @@
 public class Tic {
 
     public static String emptyboard(int indexI, int indexJ) {
-        return null;
-        /*
-         * String[][] board=null;
-         * 
-         * for(int i=0; i<indexI; i++){
-         * 
-         * for(int j=0; j<indexJ; j++){ board[i][j]="_"; } } return board;
-         */
+
+        String[][] board = new String[indexI][indexJ];
+        for (int i = 0; i < indexI; i++) {
+            for (int j = 0; j < indexJ; j++) {
+                board[i][j] = "_";
+            }
+        }
+
+        return board[indexI-1][indexJ-1];
 
     }
 
