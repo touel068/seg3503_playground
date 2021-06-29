dcdde59ad1c2c99b0c4cdf62b71278cf122006eb
Alex Degrace <adegr091@uottawa.ca>
2021-06-29 18:46:20 -0400

    Code after fourth test written

--- lab05/twitter/test/TwitterTest.java
+++ lab05/twitter/test/TwitterTest.java
@@ -108,10 +108,21 @@ class TwitterTest {
       assertEquals(false, actual);
     }
 
-    /*@Test
+    @Test
     void isMentionned_handleNull() {
-      // Assuming no tweet is available (i.e. null)
-      // isMentionned("me") should be false
-      // isMentionned("meat") should be false
-    }*/
+        Twitter twitter = partialMockBuilder(Twitter.class)
+        .addMockedMethod("loadTweet")
+        .createMock();
+
+      expect(twitter.loadTweet()).andReturn(null).times(2);
+      replay(twitter);
+
+      boolean actual;
+
+      actual = twitter.isMentionned("me");
+      assertEquals(false, actual);
+
+      actual = twitter.isMentionned("meat");
+      assertEquals(false, actual);
+    }
 }
