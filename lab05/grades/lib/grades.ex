89fa7f7781bc5c1bc6a5028efe7c8eb5cf30dff1
Thomas Ouellette <43257395+touel068@users.noreply.github.com>
2021-07-01 17:33:49 -0400

    Grades commit 1: adding functional code from assignement 2

--- lab05/grades/lib/grades.ex
+++ lab05/grades/lib/grades.ex
@@ -1,9 +1,98 @@
 defmodule Grades do
-  @moduledoc """
-  Grades keeps the contexts that define your domain
-  and business logic.
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+end
+
+defmodule Grades.Calculator do
+
+  def avg(marks) do
+    if Enum.count(marks) == 0 do
+        0
+    else
+      Enum.sum(marks) / Enum.count(marks)
+    end
+  end
+
+  def failure_to_participate(avg_homework, avg_exams, num_labs) do
+    avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3
+  end
+
+  def calculate_grade(avg_labs, avg_homework, midterm, final) do
+    0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+  end
+
+  def avg_exams(midterm,final) do
+    (midterm+final)/2
+  end
+
+  def num_labs(labs) do
+    labs
+        |> Enum.reject(fn mark -> mark < 0.25 end)
+        |> Enum.count()
+  end
+
+  def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
+
+    mark = calculate_grade(avg_labs, avg_homework, midterm, final)
+    round(mark * 100)
+  end
+
+  def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
+
+    avg_exams = avg_exams(midterm,final)
+
+    num_labs = num_labs(labs)
+
+    if failure_to_participate(avg_homework, avg_exams, num_labs) do
+      "EIN"
+    else
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
+
+      cond do
+        mark > 0.895 -> "A+"
+        mark > 0.845 -> "A"
+        mark > 0.795 -> "A-"
+        mark > 0.745 -> "B+"
+        mark > 0.695 -> "B"
+        mark > 0.645 -> "C+"
+        mark > 0.595 -> "C"
+        mark > 0.545 -> "D+"
+        mark > 0.495 -> "D"
+        mark > 0.395 -> "E"
+        :else -> "F"
+      end
+    end
+  end
+
+  def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
+
+    avg_exams = avg_exams(midterm,final)
+
+    num_labs = num_labs(labs)
+
+    if failure_to_participate(avg_homework, avg_exams, num_labs) do
+      0
+    else
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
+
+      cond do
+        mark > 0.895 -> 10
+        mark > 0.845 -> 9
+        mark > 0.795 -> 8
+        mark > 0.745 -> 7
+        mark > 0.695 -> 6
+        mark > 0.645 -> 5
+        mark > 0.595 -> 4
+        mark > 0.545 -> 3
+        mark > 0.495 -> 2
+        mark > 0.395 -> 1
+        :else -> 0
+      end
+    end
+  end
 end
